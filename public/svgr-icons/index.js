da39cc75b6af2b8a89c72b38e496d54eb10bf9cf
amir-9 <aghaamirjan13@gmail.com>
2025-01-26 02:36:21 +0330

    made components from svgs

---
+++ public/svgr-icons/index.js
@@ -0,0 +1,4 @@
+export { default as BuyIcon } from "./Buy";
+export { default as FilterIcon } from "./Filter";
+export { default as SearchNormalIcon } from "./SearchNormal";
+export { default as UserIcon } from "./User";
