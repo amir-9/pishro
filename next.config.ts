a392c5a0a4e616b137fec15588a590e0912b9be3
amir-9 <aghaamirjan13@gmail.com>
2025-01-28 14:45:35 +0330

    trying to fix fonts on server

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,32 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        // اضافه کردن این بخش
+        source: "/fonts/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
     ];
+  },
+  // اضافه کردن این بخش
+  webpack: (config, { isServer: _isServer }) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/_next/static/fonts/",
+          outputPath: "static/fonts/",
+        },
+      },
+    });
+    return config;
   },
 };
 
