0c4044f326f7aa9cb9fc3a34a641189fb035d89b
amir-9 <aghaamirjan13@gmail.com>
2025-01-28 14:51:32 +0330

    new next config

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/icons/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        // اضافه کردن این بخش
-        source: "/fonts/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-  // اضافه کردن این بخش
-  webpack: (config, { isServer: _isServer }) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]",
-          publicPath: "/_next/static/fonts/",
-          outputPath: "static/fonts/",
-        },
-      },
-    });
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
