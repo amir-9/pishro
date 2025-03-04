6a72c1011a485ccaa74e7d44f868ffc1fba336c4
amir-9 <aghaamirjan13@gmail.com>
2025-03-04 15:39:16 +0330

    changed navbar menu style

--- components/navbar/navbarPopover.tsx
+++ components/navbar/navbarPopover.tsx
@@ -32,19 +32,24 @@ const NavbarPopover = ({ item }: NavbarPopoverProps) => {
       </PopoverTrigger>
       <PopoverContent
         align="start"
-        className="bg-white text-gray-800 min-w-[80px] w-fit shadow-lg rounded-sm z-50 mt-2 p-3"
+        className={`
+          flex flex-col gap-3
+          bg-[#f3f4f6] text-gray-800
+          py-5 pr-3 pl-7 mt-2 min-w-[80px] w-fit 
+          shadow-lg rounded-lg z-50
+          `}
       >
         {item.data.map((subItem, subIdx: number) => (
           <Link
             key={subIdx}
             href={subItem.link}
             className={cn(
-              "block p-1 hover:bg-gray-100 text-xs",
-              subIdx !== item.data.length - 1 ? "border-b" : ""
+              "block p-1 relative text-[#110a3b] hover:bg-gray-100 text-sm transition duration-300 group w-fit"
             )}
             onClick={() => setIsOpen(false)}
           >
             {subItem.label}
+            <span className="absolute bottom-[-6px] left-0 w-full h-[2px] bg-[rgba(26,10,59,0.4)] origin-bottom-right scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100" />
           </Link>
         ))}
       </PopoverContent>
