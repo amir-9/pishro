67b8fb60c19d66dcd826a8af6d40719e4f0da6d5
amir-9 <aghaamirjan13@gmail.com>
2025-07-29 03:07:04 +0330

    added nft page

--- components/nft/pageContent.tsx
+++ components/nft/pageContent.tsx
@@ -1,14 +1,19 @@
-import LandingVideo from "@/components/utils/landingVideo";
 import Courses from "@/components/utils/courses";
 import Blog from "@/components/utils/blog";
 import VideoSection from "../utils/videoSection";
-import SectionOne from "./sectionOne";
+import Landing2 from "../utils/Landing2";
+import AboutIt from "./aboutIt";
+import Questions from "../faq/questions";
 
 const NftPageContent = () => {
   return (
     <div>
-      <LandingVideo vidSrc="/videos/nft.webm" />
-      <SectionOne />
+      <Landing2
+        imageUrl="/images/nft/landing-img.png"
+        title="به دنیای NFT خوش آمدید"
+      />
+      <AboutIt />
+      <Questions />
       <VideoSection videoUrl="/videos/nft.webm" label="معرفی NFT" />
       <Courses />
       <Blog />
