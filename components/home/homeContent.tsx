a0f29caf4dcfa27d3950d210a33ce9be939e106c
amir-9 <aghaamirjan13@gmail.com>
2025-10-05 17:41:42 +0330

    cleaned home component

--- components/home/homeContent.tsx
+++ components/home/homeContent.tsx
@@ -1,4 +1,3 @@
-// homePageContent.tsx
 import LandingOverlay from "./landingOverlay";
 import BikeSection from "./bikeSection";
 import MobileScrollSection from "./mobileScrollSection";
@@ -7,27 +6,16 @@ import CommentsSlider from "@/components/utils/CommentsSlider";
 import StepsSection from "./stepsSection";
 import NewsClub from "./newsClub";
 import { stepsData } from "@/public/data";
-// import GradientDivider from "./gradientDivider";
 
 const HomePageContent = () => {
   return (
     <div>
       <LandingOverlay />
-      {/* <GradientDivider /> */}
       <div className="w-full bg-white">
         <BikeSection />
         <MobileScrollSection />
         <Courses />
-
-        {/* Steps Section */}
-        <StepsSection
-          steps={stepsData}
-          sectionQuote="مسیر یادگیری خود را با پیشرو آغاز کنید"
-          sectionTitle="مراحل یادگیری در پیشرو"
-          sectionSubtitle="با طی کردن این مراحل به صورت قدم‌به‌قدم، می‌توانید مسیر یادگیری خود را کامل کنید. صورت قدم‌به‌قدم، پیشرو باشید و می‌توانید مسیر یادگیری خود را کامل کنید"
-          sectionCta="شروع مسیر یادگیری"
-        />
-
+        <StepsSection {...stepsData} />
         <CommentsSlider />
         <NewsClub />
       </div>
