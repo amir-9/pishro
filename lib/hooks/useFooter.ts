a8761e43929f3d16195a2a60a25436f3cfd8b8b3
amir-9 <aghaamirjan13@gmail.com>
2025-11-16 16:50:10 +0330

    added footer

--- lib/hooks/useFooter.ts
+++ lib/hooks/useFooter.ts
@@ -6,7 +6,7 @@ export function useFooter() {
   const pathname = usePathname();
 
   // مسیرهایی که نباید فوتر نمایش داده شود
-  const hiddenPaths = ["/investment-plans", "/business-consulting"];
+  const hiddenPaths = ["/business-consulting"];
 
   // اگر در یکی از مسیرهای بالا هستیم، false برگردان
   const showFooter = !hiddenPaths.includes(pathname);
