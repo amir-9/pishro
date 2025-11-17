371a8d00f660aa067f87cc6bcdd37c4db70ae6da
amir-9 <aghaamirjan13@gmail.com>
2025-11-17 16:32:28 +0330

    dont show footer in skyroom page

--- lib/hooks/useFooter.ts
+++ lib/hooks/useFooter.ts
@@ -6,7 +6,7 @@ export function useFooter() {
   const pathname = usePathname();
 
   // مسیرهایی که نباید فوتر نمایش داده شود
-  const hiddenPaths = ["/business-consulting"];
+  const hiddenPaths = ["/business-consulting", "/skyroom-classes"];
 
   // اگر در یکی از مسیرهای بالا هستیم، false برگردان
   const showFooter = !hiddenPaths.includes(pathname);
